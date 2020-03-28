d82836c36ec9a7d5624866d90e6f7d48f5f33786
Nicholas Boudreau <nick.boudreau89@gmail.com>
2020-03-28 14:17:25 -0400

    Created migration to add roles to user table and created isAdmin method

---
+++ app/Models/User.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Laravel\Passport\HasApiTokens;
+
+class User extends Authenticatable
+{
+	use HasApiTokens, Notifiable;
+
+	/**
+	 * The attributes that are mass assignable.
+	 *
+	 * @var array
+	 */
+	protected $fillable = [
+		'name',
+		'email',
+		'password',
+		'roles'
+	];
+
+	/**
+	 * The attributes that should be hidden for arrays.
+	 *
+	 * @var array
+	 */
+	protected $hidden = [
+		'password',
+		'remember_token',
+	];
+
+	/**
+	 * The attributes that should be cast to native types.
+	 *
+	 * @var array
+	 */
+	protected $casts = [
+		'email_verified_at' => 'datetime',
+	];
+
+	/**
+	 * Verifies if the user has the ADMIN role
+	 *
+	 * @return bool
+	 */
+	public function isAdmin()
+	{
+		if (in_array("ADMIN", $this->roles)) {
+			return true;
+		}
+
+		return false;
+	}
+}
